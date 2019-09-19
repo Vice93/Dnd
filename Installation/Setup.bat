56d3b6bb24e22e8e43ff1725ed2dbce5bf009605
Jonas Vestgarden <jonasmohammed@hotmail.no>
2019-09-19 21:09:22 +0200

    Changed script to fix pause after install of node modules

--- Installation/Setup.bat
+++ Installation/Setup.bat
@@ -2,14 +2,8 @@
 @echo off
 Powershell.exe -ExecutionPolicy RemoteSigned -File MySqlSetup.ps1
 
-echo Trying to install app modules...
 cd ..
 cd App
-npm install
 
-cd ..
-cd Installation
-
-echo Modules installert
-
-pause
+echo Trying to install app modules...
+npm install && cd .. && cd Installation && echo Modules Installed && pause
