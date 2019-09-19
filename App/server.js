12457fede1edda93fcd1f34c43f0759f32f8d17c
Jonas Vestgarden <jonasmohammed@hotmail.no>
2019-09-19 21:45:42 +0200

    Installed node-linq
    Did a few tests with it in server.js to see how it works, seems pretty good

---
+++ App/server.js
@@ -0,0 +1,25 @@
+const LINQ = require('node-linq').LINQ;
+const path = require('path');
+
+let files = ['test.txt', 'choni.txt', 'legacy.zip', 'secrets.txt', 'etc.rar'];
+let arr = new LINQ(files)
+  .Where((file) => { return path.extname(file) === '.txt'; })
+  .OrderBy((file) => { return file;})
+  .ToArray();
+
+arr.forEach(elem => {
+    console.log("Elem: " + elem);
+});
+
+
+let bool = new LINQ(arr).Any((file) => { return file.includes('test'); });
+
+console.log(bool);
+
+let first = new LINQ(arr).First((file) => { return file.includes('test'); });
+
+console.log(first);
+
+let notFound = new LINQ(arr).First((file) => { return file.includes('error'); });
+
+console.log(notFound);
