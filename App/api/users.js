4e85850585f6933db5de85854793f6294c36438b
Jonas Kristoffer Vestgarden <m76036@TIW0KOSS-APP11>
2019-09-23 15:41:55 +0200

    Not supposed to commit the commented code

--- App/api/users.js
+++ App/api/users.js
@@ -1,4 +1,4 @@
-//const mysql = require('../database/initPool.js')
+const mysql = require('../database/initPool.js')
 const User = require('../model/User.js')
 
 module.exports = (router) => {
@@ -8,17 +8,17 @@ module.exports = (router) => {
   // For instance, this one listens on the route defined in app.js ('/api/users'), and here we can specify further paths
   // This one now processes GET requests on the endpoint: 'http://localhost:3001/api/users/'
   router.get('/', (req,res) => {
-    // Use mysql to query 'users' table, select everything then map the result to our User object
-    // mysql('users').select().map((user) => new User(user)).then((users) => {
-    //   users.forEach(element => {
-    //     // We can use functions defined on the model if we want to
-    //     element.testFunction()
-    //   })
-    //   return res.json({ success: true, data: users })
-    // }).catch((ex) => {
-    //   console.log(ex)
-    //   return res.json({ success: false, err: ex })
-    // })
+    //Use mysql to query 'users' table, select everything then map the result to our User object
+    mysql('users').select().map((user) => new User(user)).then((users) => {
+      users.forEach(element => {
+        // We can use functions defined on the model if we want to
+        element.testFunction()
+      })
+      return res.json({ success: true, data: users })
+    }).catch((ex) => {
+      console.log(ex)
+      return res.json({ success: false, err: ex })
+    })
     return res.json({success:true, data: null})
   })
 
