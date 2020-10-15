2a9a89612537dc03529288438efcc8e5cf1552cc
Jonas Vestgarden <jonasmohammed@hotmail.no>
2020-10-15 18:44:26 +0200

    fix environment variables

--- app.js
+++ app.js
@@ -1,38 +1,27 @@
 /* Imports */
 const express = require('express')
-const cors = require('cors')
 const bodyParser = require('body-parser')
 const logger = require('morgan')
-const appenv = require('./appenv.js')
 
 /**
 * Initialize the application using express 
 */
 const app = express()
-/** 
-* Use cors to allow cross-domain traffic (Frontend/Backend traffic). 
-*/
-app.use(cors({
-  origin: [
-    `${appenv.domain}`,
-    '<other ips you want to allow>',
-  ],
-  optionsSuccessStatus:200 // Optional, but you can define http status code for success here, 200 is the standard
-}))
+
 /** 
 * Use bodyparser to parse the requests/responses we send. We are mostly using json, but some formdata is sent as urlencoded so we support both these. 
 * There are also various more, but we wont be using those 
 */
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
-app.use(logger(appenv.env))
+app.use(logger(process.env.NODE_ENV))
 /**
  * Next we need to define our routes. These are our api end-points where we listen to requests from react
  */
 const router = express.Router()
-const usersApi = require('./api/users.js')(router)
-app.use('/api', usersApi)
+//const usersApi = require('./api/users.js')(router)
+//app.use('/api', usersApi)
 /**
  * Finally we tell the app to listen to all traffic on the specified port
  */
-app.listen(appenv.api_port, () => console.log(`Listening on port ${appenv.api_port}`))
+app.listen(process.env.NODE_ENV, () => console.log(`Listening on port ${process.env.NODE_ENV}`))
